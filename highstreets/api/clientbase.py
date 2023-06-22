405ebc92508fb9abfef23934e9446aa593b7c049
Anupam Bose <anupam.bose@london.gov.uk>
2023-06-22 14:56:01 +0100

    all requests put in clientbase

--- highstreets/api/clientbase.py
+++ highstreets/api/clientbase.py
@@ -1,6 +1,7 @@
 import base64
 import logging
 import os
+import uuid
 
 import requests
 from dotenv import find_dotenv, load_dotenv
@@ -12,8 +13,15 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 
+class APIClientException(Exception):
+    pass
+
+
 class APIClient:
     token_endpoint = "https://api.business.bt.com/oauth/accesstoken"  # noqa: S105
+
+    def __init__(self):
+        self.token = self.get_access_token()
 
     @staticmethod
     def get_access_token():
@@ -46,4 +54,40 @@ class APIClient:
             return token
         except requests.exceptions.RequestException as e:
             logger.error("Failed to obtain access token: %s", str(e))
-            raise
+            raise APIClientException("Failed to obtain access token.") from None
+
+    def get_data_request(self, endpoint, headers=None, params=None):
+        headers = headers or {}
+        headers["Authorization"] = f"Bearer {self.token}"
+        headers["APIGW-Tracking-Header"] = str(uuid.uuid4())
+
+        all_data = []  # List to store the complete data from all pages
+        current_page = 1
+
+        while True:
+            try:
+                params["page"] = current_page
+                response = requests.get(
+                    endpoint, headers=headers, params=params, timeout=5
+                )
+                response.raise_for_status()
+
+                data = response.json().get("data")
+                all_data.extend(data)
+
+                pagination_metadata = response.json().get("pagination_metadata")
+                next_page_number = pagination_metadata.get("next_page_number")
+
+                if next_page_number is not None:
+                    current_page = next_page_number
+                else:
+                    break
+
+            except requests.exceptions.RequestException as e:
+                logger.error(f"Request exception occurred: {str(e)}")
+                raise APIClientException("Failed to make the request.") from None
+            except Exception as e:
+                logger.error(f"An unexpected error occurred: {str(e)}")
+                raise APIClientException("An unexpected error occurred.") from None
+
+        return all_data
--- highstreets/data_source_sink/dataloader.py
+++ highstreets/data_source_sink/dataloader.py
@@ -1,51 +1,26 @@
 import logging
-import uuid
 
-import requests
+from highstreets.api.clientbase import APIClient, APIClientException
 
-# from highstreets.api.client_base import APIClient
+
+class DataLoaderException(Exception):
+    pass
 
 
 class DataLoader:
-    def __init__(self, api_endpoint, token):
+    def __init__(self, api_endpoint):
         self.api_endpoint = api_endpoint
-        self.token = token
+        self.api_client = APIClient()
         self.logger = logging.getLogger(__name__)
 
     def get_hex_data(self, date_from, date_to):
         params = {"date_from": date_from, "date_to": date_to}
-        all_data = []  # List to store the complete data from all pages
-        current_page = 1
 
-        while True:
-            try:
-                params["page"] = current_page
-                headers = {
-                    "Authorization": f"Bearer {self.token}",
-                    "APIGW-Tracking-Header": str(uuid.uuid4()),
-                }
-                response = requests.get(
-                    self.api_endpoint, headers=headers, params=params, timeout=5
-                )
-                response.raise_for_status()  # Raise exception for unsuccessful requests
-
-                data = response.json().get("data")
-                all_data.extend(data)
-
-                pagination_metadata = response.json().get("pagination_metadata")
-                next_page_number = pagination_metadata.get("next_page_number")
-
-                if next_page_number is not None:
-                    current_page = next_page_number
-                else:
-                    break
-
-            except requests.exceptions.RequestException as e:
-                self.logger.error(f"Request exception occurred: {str(e)}")
-                break
-
-            except Exception as e:
-                self.logger.error(f"An unexpected error occurred: {str(e)}")
-                break
-
-        return all_data
+        try:
+            return self.api_client.get_data_request(self.api_endpoint, params=params)
+        except APIClientException as e:
+            self.logger.error(str(e))
+            raise DataLoaderException("Failed to fetch data.") from None
+        except Exception as e:
+            self.logger.error(f"An unexpected error occurred: {str(e)}")
+            raise DataLoaderException("An unexpected error occurred.") from None
