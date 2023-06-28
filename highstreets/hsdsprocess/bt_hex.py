e1a8d074a0875f67eaca804e2fd036c151e30c9e
Anupam Bose <anupam.bose@london.gov.uk>
2023-06-28 12:24:53 +0100

    dates parameter passed as env variables from jenkins

--- highstreets/hsdsprocess/bt_hex.py
+++ highstreets/hsdsprocess/bt_hex.py
@@ -7,10 +7,12 @@ data_loader = DataLoader(
     "https://api.business.bt.com/v1/footfall/reports/hex-grid/tfl?agg=time_indicator"
 )
 
+# Get the start_date and end_date
 start_date = os.environ.get("START_DATE")
 end_date = os.environ.get("END_DATE")
+
 print(start_date)
-data = data_loader.get_hex_data("2023-05-10", "2023-05-10")
+data = data_loader.get_hex_data(start_date, end_date)
 hex_transform = HexTransform()
 transformed_data = hex_transform.transform_data(data)
 print(transformed_data)
