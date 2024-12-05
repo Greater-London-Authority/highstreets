2babf6f1a55a4c17ae8cbf2068aedbda1cd3d41f
AnupamBose1997 <anupam.bose21@gmail.com>
2024-12-05 14:23:14 +0000

    test change

--- highstreets/hsdsprocess/mcard_weekly.py
+++ highstreets/hsdsprocess/mcard_weekly.py
@@ -7,6 +7,8 @@ from highstreets.data_transformation.mcard_transform import McardTransform
 
 base_dir = config.BASE_DIR
 
+a = base_dir
+
 # instantiate the classes
 data_loader = DataLoader()
 data_writer = DataWriter()
